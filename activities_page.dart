7ec30d9fe64dbb713bc22824f5dd8ffd8c8c4273
Vivek9873 <103317330+Vivek9873@users.noreply.github.com>
2022-06-04 15:53:21 +0530

    Add files via upload

---
+++ activities_page.dart
@@ -0,0 +1,150 @@
+import 'package:awesome_app/assignment_page.dart';
+import 'package:awesome_app/classes_page.dart';
+import 'package:awesome_app/clubs_page.dart';
+import 'package:awesome_app/quizzes_page.dart';
+import 'package:awesome_app/webinars_page.dart';
+import 'package:awesome_app/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ActivityPage extends StatelessWidget {
+  const ActivityPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Activities Page",style: TextStyle(
+          fontSize: 28.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+
+        ),),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child:Container(
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.all(0),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: Size.fromHeight(146),
+                    textStyle: TextStyle(fontSize: 28,fontWeight:FontWeight.bold ),
+                    primary: Colors.black,
+                    side: BorderSide(width: 3,color: Colors.black),
+                    // onPrimary: Colors.black,
+                  ),
+                  child: Text('Assignments'),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .push(
+                      MaterialPageRoute(builder: (context) => AssignmentPage()
+                      )
+                    );
+                  }
+                ),
+
+
+              ),
+          ),
+
+
+          Expanded(
+            child: Container(
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(0),
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  minimumSize: Size.fromHeight(146),
+                  textStyle: TextStyle(fontSize: 28,fontWeight:FontWeight.bold ),
+                  primary: Colors.black,
+                  side: BorderSide(width: 3,color: Colors.black),
+                  // onPrimary: Colors.black,
+                ),
+                child: Text('Quizzes'),
+                onPressed: ()  {
+                  Navigator.of(context)
+                      .push(
+                      MaterialPageRoute(builder: (context) => QuizzesPage()
+                      )
+                  );
+                }
+              ),
+            ),
+          ),
+         Expanded(
+           child:  Container(
+             alignment: Alignment.topCenter,
+             padding: EdgeInsets.all(0),
+             child: OutlinedButton(
+               style: OutlinedButton.styleFrom(
+                 minimumSize: Size.fromHeight(146),
+                 textStyle: TextStyle(fontSize: 28,fontWeight:FontWeight.bold ),
+                 primary: Colors.black,
+                 side: BorderSide(width: 3,color: Colors.black),
+                 // onPrimary: Colors.black,
+               ),
+               child: Text('Webinars'),
+               onPressed: ()  {
+                 Navigator.of(context)
+                     .push(
+                     MaterialPageRoute(builder: (context) => WebinarsPage()
+                     )
+                 );
+               }
+             ),
+
+
+           ),
+         ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.all(0),
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  minimumSize: Size.fromHeight(146),
+                  textStyle: TextStyle(fontSize: 28,fontWeight:FontWeight.bold ),
+                  primary: Colors.black,
+                  side: BorderSide(width: 3,color: Colors.black),
+                  // onPrimary: Colors.black,
+                ),
+                child: Text('Classes'),
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(
+                      MaterialPageRoute(builder: (context) => ClassesPage()
+                      )
+                  );
+                }
+              ),
+            ),
+          ),
+         Expanded(child:  Container(
+           alignment: Alignment.topCenter,
+           padding: EdgeInsets.all(0),
+           child: OutlinedButton(
+             style: OutlinedButton.styleFrom(
+               minimumSize: Size.fromHeight(146),
+               textStyle: TextStyle(fontSize: 28,fontWeight:FontWeight.bold ),
+               primary: Colors.black,
+               side: BorderSide(width: 3,color: Colors.black),
+               // onPrimary: Colors.black,
+             ),
+             child: Text('Clubs'),
+             onPressed: () {
+               Navigator.of(context)
+                   .push(
+                   MaterialPageRoute(builder: (context) => ClubsPage()
+                   )
+               );
+             }
+           ),
+         )),
+        ],
+      ),
+      drawer: const MyDrawer(),
+    );
+  }
+}
